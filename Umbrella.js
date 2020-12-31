a9a5586b8cd6529beb6b783efedd97ba97fbbefc
DesaiSwara <68277364+DesaiSwara@users.noreply.github.com>
2020-12-31 11:46:23 +0530

    Add files via upload

--- Umbrella.js
+++ Umbrella.js
@@ -16,7 +16,14 @@ class Umbrella
 		this.r=r;
 		this.body=Bodies.circle(this.x, this.y, this.r/2, options);
 	
-        this.image=loadImage("Boy with umbrella.jpg")
+		this.image=loadImage("images/Walking Frame/walking_1.png");
+		this.image=loadImage("images/Walking Frame/walking_2.png");
+		this.image=loadImage("images/Walking Frame/walking_3.png");
+		this.image=loadImage("images/Walking Frame/walking_4.png");
+		this.image=loadImage("images/Walking Frame/walking_5.png");
+		this.image=loadImage("images/Walking Frame/walking_6.png");
+		this.image=loadImage("images/Walking Frame/walking_7.png");
+		this.image=loadImage("images/Walking Frame/walking_8.png");
 		World.add(world, this.body);
 		
 	}
@@ -30,7 +37,8 @@ class Umbrella
 			//strokeWeight(4);
 			fill(255,0,255);
 			imageMode(CENTER)
-	        image(this.image, 0, 0, 200, 250)
+			image(this.image, 0, 0, 200, 210);
+			
 			//ellipse(0,0,this.r, this.r);
 			pop()
 			
--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+const Events = Matter.Events
 
-
-var maxdrops=100,drops,umbrella;
+var maxdrops=100,umbrella;
+var drops=[];
 
 function preload(){
     
@@ -16,17 +17,20 @@ function setup(){
     engine = Engine.create();
 	world = engine.world;
 	
-    umbrella=new Umbrella(200,610,50,50);
-    for(var i=0; i<maxdrops; i++){
-      drops.push(new Drops(random(0,400), random(0,400)));
-    }
+    umbrella=new Umbrella(200,610,75,75);
 
+    for(var i=0; i<maxdrops; i++) {
+         drops.push(new Drops(random(0,400), random(0,400)));
+    }
 	Engine.run(engine);  
 }
 
 function draw(){
-    background(225)
+    background(0)
     umbrella.display();
-    drops.display();
+
+    for (var i = 0; i < drops.length; i++) {
+          drops[i].display;
+    }
 }   
 
